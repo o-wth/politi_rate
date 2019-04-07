302e53d182eeb6fb40cbdee4aea4a3a6d82c4708
autinmitra <autin123@gmail.com>
2019-04-07 07:11:57 -0400

    AUTIN's API

---
+++ api/endpoints.js
@@ -0,0 +1,8 @@
+module.exports = {
+    aylien: 'https://api.aylien.com/api/v1',
+    aylienKey: '71955aafb5e10acc76ec252589ba9fc9',
+    aylienID: 'fe19b1b6',
+    phone2action: 'https://fmrrixuk32.execute-api.us-east-1.amazonaws.com/hacktj/legislators',
+    phone2actionKey: 'ie5EtNqb2pafUpw0FsMC84hHqrW9L4uf2Ql9YTJF',
+
+}
