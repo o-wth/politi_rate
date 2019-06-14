7a628cdb2c29cdadb4bce07d74fccacce6762c4f
sumanthratna <sumanthratna@gmail.com>
2019-06-14 18:27:53 -0400

    Hide keys

--- api/twitterKeys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'VrSFJu4LVeiUFgFbtryjfTZzH',
-    consumer_secret: 'jAtcjNU5UDQb2j8hD06tjP3KJklW4QL0wIfDS1cQ2vXaseI6Ec',
-    access_token_key: '1073011222931496961-i3fljBS4yLiJ345pLrafWTkk68Oy7w',
-    access_token_secret: 'YwgP1tb5jDdVI8rGuPwxOqEq7RccK8w24e8npNG0BWlNc'
-}
